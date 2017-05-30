54b07f3b3eaff6eeb8897a9ff2823c3f044c8e2c
Mauro Servienti <mauro@topics.it>
2017-05-30 15:23:51 +0200

    empty Customers Registry controller

---
+++ src/CustomersRegistry.API.Host/Controllers/CustomersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace Divergent.Sales.API.Host.Controllers
+{
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+        [HttpPost]
+        public dynamic Post([FromBody]dynamic customer)
+        {
+            return 12; //new customer ID
+        }
+    }
+}
