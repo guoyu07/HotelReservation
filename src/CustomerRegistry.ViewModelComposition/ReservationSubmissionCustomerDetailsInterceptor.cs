0726beb899248c77d5dc691f85f0d820040f294e
mauroservienti <mauro@topics.it>
2017-05-30 18:35:14 +0200

    ViewModel composition basic skeleton

---
+++ src/CustomerRegistry.ViewModelComposition/ReservationSubmissionCustomerDetailsInterceptor.cs
@@ -0,0 +1,35 @@
+﻿using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersRegistry.ViewModelComposition
+{
+    class ReservationSubmissionCustomerDetailsInterceptor : IHandleRequests
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            //this is the POST interceptor when reservation is submitted
+            var controller = (string)routeData.Values["controller"];
+            var action = (string)routeData.Values["action"];
+
+            return HttpMethods.IsPost(httpVerb)
+                && controller.ToLowerInvariant() == "reservation"
+                && action.ToLowerInvariant() == "submit";
+        }
+
+        public Task Handle(dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            var form = request.Form;
+            /*
+             * Get Customer details from incoming FORM
+             * and post them to Customers Registry API
+             */
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/CustomerRegistry.ViewModelComposition/ReservationSummaryCustomerDetailsInterceptor.cs
@@ -0,0 +1,45 @@
+﻿using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersRegistry.ViewModelComposition
+{
+    class ReservationSummaryCustomerDetailsInterceptor : IHandleRequests
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            //this is the reservation summary before final confirmation by the user
+            var controller = (string)routeData.Values["controller"];
+            var action = (string)routeData.Values["action"];
+
+            return HttpMethods.IsGet(httpVerb)
+                && controller.ToLowerInvariant() == "reservation"
+                && action.ToLowerInvariant() == "summary";
+        }
+
+        public Task Handle(dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            if (request.HttpContext.User.Identity.IsAuthenticated)
+            {
+                /*
+                 * try read customer info from backend API 
+                 * based on current user id and pre-fill 
+                 * the submission form
+                 */
+            }
+            else
+            {
+                /*
+                 * add to vm an empty customer model 
+                 * ready to be filled in the form
+                 */
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
