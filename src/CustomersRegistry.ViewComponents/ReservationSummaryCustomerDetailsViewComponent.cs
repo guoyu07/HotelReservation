7d67c90fe629a664c19f5b8135662e43b0ce28c5
mauroservienti <mauro@topics.it>
2017-05-30 18:35:36 +0200

    UI Composition basic skeleton

---
+++ src/CustomersRegistry.ViewComponents/ReservationSummaryCustomerDetailsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersRegistry.ViewComponents
+{
+    public class ReservationSummaryCustomerDetailsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(dynamic model)
+        {
+            await Task.CompletedTask;
+            return View(model);
+        }
+    }
+}
