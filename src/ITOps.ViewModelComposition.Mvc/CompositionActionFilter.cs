bb642877acd9a63d5fcaa436186ce341bb66ff55
Mauro Servienti <mauro@topics.it>
2017-06-01 12:01:42 +0200

    result handling refactoring, simpler approach

--- src/ITOps.ViewModelComposition.Mvc/CompositionActionFilter.cs
+++ src/ITOps.ViewModelComposition.Mvc/CompositionActionFilter.cs
@@ -1,4 +1,6 @@
 ﻿using ITOps.ViewModelComposition.Gateway;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
 using System.Collections.Generic;
@@ -9,7 +11,6 @@ namespace ITOps.ViewModelComposition.Mvc
 {
     class CompositionActionFilter : IAsyncResultFilter
     {
-        IHandleResult defaultHandler = new DefaultResultHandler();
         IEnumerable<IHandleResult> resultHandlers;
 
         public CompositionActionFilter( IEnumerable<IHandleResult> resultHandlers )
@@ -28,11 +29,38 @@ namespace ITOps.ViewModelComposition.Mvc
             //per route only 1 result handler is allowed, the owning one
             var handler = resultHandlers
                 .Where(a => a.Matches(routeData, request.Method, request))
-                .SingleOrDefault() ?? defaultHandler;
+                .SingleOrDefault();
 
-            await handler.Handle(context, viewModel, statusCode);
+            if (handler != null)
+            {
+                await handler.Handle(context, viewModel, statusCode);
+            }
+            else
+            {
+                defaultHandler();
+            }
 
             await next();
+
+            void defaultHandler()
+            {
+                if (context.Result is ViewResult viewResult && viewResult.ViewData.Model == null)
+                {
+                    //MVC
+                    if (statusCode == StatusCodes.Status200OK)
+                    {
+                        viewResult.ViewData.Model = viewModel;
+                    }
+                }
+                else if (context.Result is ObjectResult objectResult && objectResult.Value == null)
+                {
+                    //WebAPI
+                    if (statusCode == StatusCodes.Status200OK)
+                    {
+                        objectResult.Value = viewModel;
+                    }
+                }
+            }
         }
     }
 }
