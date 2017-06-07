cde84969ca74cad6b9ebcc4695121bbf365d3e05
Mauro Servienti <mauro@topics.it>
2017-06-07 16:02:09 +0200

    adding missing handlers (still empty)

---
+++ src/Payments.ViewModelComposition/NewReservationDetailsHandler.cs
@@ -0,0 +1,30 @@
+﻿using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+
+namespace Payments.ViewModelComposition
+{
+    class NewReservationDetailsHandler : IHandleRequests
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            //this is the reservation summary before final confirmation by the user
+            var controller = (string)routeData.Values["controller"];
+            var action = (string)routeData.Values["action"];
+
+            return HttpMethods.IsGet(httpVerb)
+                && controller.ToLowerInvariant() == "reservation"
+                && action.ToLowerInvariant() == "new";
+        }
+
+        public Task HandleAsync(dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            /*
+             * pre-fill available data, if any, about user credit card
+             */
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Payments.ViewModelComposition/NewReservationSubmissionDetailsHandler.cs
@@ -0,0 +1,38 @@
+﻿using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Threading.Tasks;
+
+namespace Payments.ViewModelComposition
+{
+    class NewReservationSubmissionDetailsHandler : IHandleRequests
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            //this is the POST interceptor when reservation is submitted
+            var controller = (string)routeData.Values["controller"];
+            var action = (string)routeData.Values["action"];
+
+            return HttpMethods.IsPost(httpVerb)
+                && controller.ToLowerInvariant() == "reservation"
+                && action.ToLowerInvariant() == "new";
+        }
+
+        public Task HandleAsync(dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            var form = request.Form;
+            var rid = request.Form["ReservationId"][0];
+
+            var reservationId = new Guid(rid);
+
+            /*
+             * using credit card details from the FORM
+             * post them to the Payments WebAPI to start
+             * the payment process
+             */
+
+            return Task.CompletedTask;
+        }
+    }
+}
