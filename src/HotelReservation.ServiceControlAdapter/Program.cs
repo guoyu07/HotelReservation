41182de36362bb3921a19d3b6f4a660c183ac51b
mauroservienti <mauro@topics.it>
2017-06-13 12:51:47 +0200

    ServiceControl transport adapter, fix #3

---
+++ src/HotelReservation.ServiceControlAdapter/Program.cs
@@ -0,0 +1,38 @@
+﻿using NServiceBus;
+using NServiceBus.Configuration.AdvanceExtensibility;
+using ServiceControl.TransportAdapter;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelReservation.ServiceControlAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            var adapterName = typeof(Program).Namespace;
+            Console.Title = adapterName;
+
+            var config = new TransportAdapterConfig<LearningTransport, MsmqTransport>(adapterName);
+            config.CustomizeServiceControlTransport(c =>
+            {
+                c.GetSettings().Set("errorQueue", "error");
+            });
+
+            var adapter = TransportAdapter.Create(config);
+            await adapter.Start().ConfigureAwait(false);
+
+            Console.WriteLine();
+            Console.WriteLine($"{adapterName} created and configured; press any key to stop program.");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            await adapter.Stop().ConfigureAwait(false);
+        }
+    }
+}
