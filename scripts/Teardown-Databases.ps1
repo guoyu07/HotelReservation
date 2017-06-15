b2fa94ba23b9325001fc2fe2ec0be078ce705438
Mauro Servienti <mauro@topics.it>
2017-06-15 10:47:15 +0200

    database creation and teardown scripts

---
+++ scripts/Teardown-Databases.ps1
@@ -0,0 +1,9 @@
+﻿#Requires -RunAsAdministrator
+
+$instanceName = "hotel-reservation"
+
+$serverName = "(localdb)\" + $instanceName
+sqlcmd -S $serverName -i ".\Teardown-Databases.sql"
+
+sqllocaldb stop $instanceName
+sqllocaldb delete $instanceName
